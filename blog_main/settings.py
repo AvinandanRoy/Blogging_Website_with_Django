578e014dd131973e35d7691e1ad3fd76a61cffa3
AvinandanRoy <abhinandanroylipi@gmail.com>
2026-04-30 13:17:30 +0600

    DATABASES change in settings.py and also freeze erequirements.txt file

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -84,19 +84,11 @@ WSGI_APPLICATION = 'blog_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        # If RAILWAY_ENVIRONMENT is not found, it defaults to local SQLite
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
